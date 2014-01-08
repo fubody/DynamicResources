a2f160ddb55c418ae81d5df18c1b2a1f1c26217e
fubody17 <fubody17@163.com>
2014-01-08 11:41:21 +0800

    modify

--- test.bat
+++ test.bat
@@ -1,3 +1,3 @@
-git add .
-git commit -m "modify"
-git push git@github.com:fubody/DynamicResources.git
+"C:\Program Files (x86)\Git\bin\git" add .
+"C:\Program Files (x86)\Git\bin\git" commit -m "modify"
+"C:\Program Files (x86)\Git\bin\git" push git@github.com:fubody/DynamicResources.git
